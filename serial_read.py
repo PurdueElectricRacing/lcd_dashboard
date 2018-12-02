e4f43ab5bf672e5d232e11b1fa515d041f41ef53
mflanag <mflanag@purdue.edu>
2018-12-02 13:36:01 -0500

    added nicer user features to ui

--- serial_read.py
+++ serial_read.py
@@ -4,7 +4,9 @@ import time
 if __name__ == "__main__":
     
     while 1:
-        with serial.Serial('COM1', 9600, timeout=1) as ser:
+        with serial.Serial('COM11', 9600, timeout=1) as ser:
             x = ser.readline()
             print(x)
             #ser.write(b'sup')
+            #print("writing data")
+            #ser.write('hello!\n'.encode('utf-8'))
